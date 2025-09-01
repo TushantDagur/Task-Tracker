3596247443586206aceab7114375fb4953ba84d7
TushantDagur <Tushantdagur125@gmail.com>
2025-09-01 22:32:40 +0530

    Final Commit

--- task-tracker/src/components/Task.js
+++ task-tracker/src/components/Task.js
@@ -1,13 +1,15 @@
-import { FaTimes } from 'react-icons/fa'
-
 const Task = ({ task, onDelete, onToggle }) => {
     return (
         <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)} >
-            <h3>{task.text} 
-                <FaTimes style={{ color:'red',cursor:'pointer'}} 
-                    onClick={() => onDelete(task.id
-                )}
-                />
+            <h3>
+                {task.text}{' '}
+                {/* FIX: Replaced the FaTimes icon component with a simple 'X' text to remove the dependency */}
+                <span
+                    style={{ color: 'red', cursor: 'pointer', fontWeight: 'bold' }}
+                    onClick={() => onDelete(task.id)}
+                >
+                    X
+                </span>
             </h3>
             <h3>{task.day}</h3>
         </div>
